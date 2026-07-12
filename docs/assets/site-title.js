cf3111d5d4760d084b52b482c132e24a7580c7dd
Jose Pino <pepepinoortega@gmail.com>
2026-07-12 16:37:45 +0200

    Deployed 9f72b57 with MkDocs version: 1.6.1

---
+++ docs/assets/site-title.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.title = "SportsLabResearch";
+});
+
+document.addEventListener("DOMContentSwitch", function () {
+    document.title = "SportsLabResearch";
+});
